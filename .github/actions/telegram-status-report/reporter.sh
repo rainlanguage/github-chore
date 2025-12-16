3c13b35260aa464788387ea4e0ff2215c404ac0a
rouzwelt <rouzweltt@gmail.com>
2025-12-16 03:19:21 +0000

    update

--- .github/actions/telegram-status-report/reporter.sh
+++ .github/actions/telegram-status-report/reporter.sh
@@ -34,6 +34,7 @@ RES=$(curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMes
 # check if sent successfully
 if echo "$RES" | grep -q '"ok":false'; then
     echo "❌ Failed to send Telegram message"
+    echo $RES
     exit 1
 fi
 
